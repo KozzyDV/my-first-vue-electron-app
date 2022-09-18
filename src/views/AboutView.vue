a6972f59c41a5f5a7c8eb468a2749d15f96db5d3
KozzyDaViking <kozzy222@gmail.com>
2022-09-18 21:46:56 +0930

    Initial filling of the about page.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Purpose</h1>
+    <p>
+      The purpose of this site is for me to learn the Vue framework and
+      integrating with Electron. Each page of this site will be me testing
+      different skills. By the end, the site will serve as an archive of my
+      learning journey.
+    </p>
+
+    <!--TODO: Make links open in OS default browser-->
+    <h1>Socials</h1>
+    <ul>
+      <li>
+        <a
+          href="https://www.instagram.com/kozzydaviking/"
+          target="_blank"
+          rel="noopener"
+          >Instagram</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/KozzyDV" target="_blank" rel="noopener">
+          Twitter</a
+        >
+      </li>
+      <li>
+        <a href="https://github.com/KozzyDV" target="_blank" rel="noopener">
+          GitHub</a
+        >
+      </li>
+    </ul>
   </div>
 </template>
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
